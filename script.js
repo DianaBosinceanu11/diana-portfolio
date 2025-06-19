9eb988e63afc62fd618398590eafe5a075b288cf
DianaBosinceanu11 <dianabosinceanu11@gmail.com>
2025-06-19 02:21:11 +0100

    Initial commit - glowing portfolio

---
+++ script.js
@@ -0,0 +1,29 @@
+// script.js
+const typingEl = document.querySelector('.typing');
+const roles = ['Frontend Developer', 'UI/UX Designer', 'Creative Thinker'];
+let index = 0;
+let charIndex = 0;
+let currentText = '';
+let isDeleting = false;
+
+function typeEffect() {
+  currentText = roles[index];
+  if (isDeleting) {
+    typingEl.textContent = currentText.substring(0, charIndex--);
+  } else {
+    typingEl.textContent = currentText.substring(0, charIndex++);
+  }
+
+  if (!isDeleting && charIndex === currentText.length) {
+    isDeleting = true;
+    setTimeout(typeEffect, 1500);
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    index = (index + 1) % roles.length;
+    setTimeout(typeEffect, 500);
+  } else {
+    setTimeout(typeEffect, isDeleting ? 50 : 100);
+  }
+}
+
+typeEffect();
